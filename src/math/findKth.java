6ebdd0dc033d48a8a5d2f7cdd791782287a63bd3
Xiaoxu <xiaoxul@andrew.cmu.edu>
2015-02-22 10:17:10 -0500

    bug fix...

--- src/math/findKth.java
+++ src/math/findKth.java
@@ -15,28 +15,29 @@ public class findKth {
 			int v3 = Integer.MAX_VALUE, v5 = Integer.MAX_VALUE,
 					v7 = Integer.MAX_VALUE;
 			if(!q3.isEmpty()) v3 = q3.peek();
-			if(!q5.isEmpty()) v5 = q3.peek();
-			if(!q3.isEmpty()) v7 = q3.peek();
+			if(!q5.isEmpty()) v5 = q5.peek();
+			if(!q7.isEmpty()) v7 = q7.peek();
 			res = Math.min(v3, Math.min(v5, v7));
 			if(res == v3){
 				q3.remove();
-				q3.add(3*v3);
-				q5.add(5*v3);
-				q7.add(7*v3);
+				q3.add(3*res);
+				q5.add(5*res);
+				q7.add(7*res);
 			}else if(res == v5){
 				q5.remove();
-				q5.add(5*v3);
-				q7.add(7*v3);
+				q5.add(5*res);
+				q7.add(7*res);
 			}else if(res == v7){
-				q5.remove();
-				q7.add(7*v3);
+				q7.remove();
+				q7.add(7*res);
 			}
 		}
 		return res;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(findk(3));
+		for(int i = 1; i < 20; i++)
+			System.out.println(findk(i));
 	}
 
 }
